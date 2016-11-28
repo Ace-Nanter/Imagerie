e624be261dd52e04eff598f5d6bcadc1c6594d26
Alexandre Rabérin <kernelith@live.fr>
2016-11-28 17:20:53 +0100

    Add mask funtion

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,47 @@
 #include <cstdlib>
 
+#include <iostream>
+#include <vector>
+
 #include "CImg.h"
 
 using namespace cimg_library;
 
-int main(int argc, char ** argv)
+/**
+ * @brief Récupère l'ensemble des coordonnées des pixels faisant l'objet d'une reconstitution.
+ * @param input Image d'entrée.
+ * @return Liste des coordonnées des pixels de l'image.
+ */
+std::vector< std::pair< unsigned int, unsigned int > > getMask(const CImg<>& input);
+
+int main(int argc, char** argv)
 {
-	CImg<unsigned char> image("images/test.bmp");
+    CImg<float> input("images/lenaGrayHidden.bmp");
+    CImgDisplay displayInput(input, "Image d'origine");
+
+    std::vector< std::pair< unsigned int, unsigned int > > mask = getMask(input);
+
+    std::cout << "Nombre de pixel à reconstituer : " << mask.size() << std::endl;
 	
-	CImgDisplay display(image, "Image");
-	
-	while (!display.is_closed())
+    while (!displayInput.is_closed())
 	{
-		display.wait();
+        displayInput.wait();
 	}
 	
     return EXIT_SUCCESS;
+}
+
+std::vector< std::pair< unsigned int, unsigned int > > getMask(const CImg<>& input)
+{
+    std::vector< std::pair< unsigned int, unsigned int > > mask;
+
+    // Ajoute tous les pixels de l'image qui doivent être reconstitués
+    cimg_forXY(input, x, y)
+    {
+        // Pixels rouges
+        if (input(x, y, 0) == 255 && input(x, y, 1) == 0 && input(x, y, 2) == 0)
+            mask.push_back({x, y});
+    }
+
+    return mask;
 }
