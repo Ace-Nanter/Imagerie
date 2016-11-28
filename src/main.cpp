e61a73b2f1a25dfa186899501900e42c49ccc117
Alexandre Rabérin <kernelith@live.fr>
2016-11-28 16:34:27 +0100

    Correct cmakelist
    test program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
+#include <cstdlib>
+
 #include "CImg.h"
+
+using namespace cimg_library;
 
 int main(int argc, char ** argv)
 {
-    return 0;
+	CImg<unsigned char> image("images/test.bmp");
+	
+	CImgDisplay display(image, "Image");
+	
+	while (!display.is_closed())
+	{
+		display.wait();
+	}
+	
+    return EXIT_SUCCESS;
 }
