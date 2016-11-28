d02f150c14b6d5ea236b53027cbc181b9dd0118f
Alexandre Rabérin <kernelith@live.fr>
2016-11-28 17:38:38 +0100

    random initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <cstdlib>
 
 #include <iostream>
+#include <random>
 #include <vector>
 
 #include "CImg.h"
 
 using namespace cimg_library;
+
+std::mt19937 mt(123456789); ///< Générateur aléatoire
 
 /**
  * @brief Récupère l'ensemble des coordonnées des pixels faisant l'objet d'une reconstitution.
@@ -14,16 +17,32 @@ using namespace cimg_library;
  */
 std::vector< std::pair< unsigned int, unsigned int > > getMask(const CImg<>& input);
 
+/**
+ * @brief Initilialise les pixels du faisant partis du mask à une valeur aléatoire tirée depuis l'image d'entrée.
+ * @param mask Vecteur des pixels du mask.
+ * @param input Image d'entrée.
+ */
+void randomInitMask(const std::vector< std::pair< unsigned int, unsigned int > >& mask, CImg<>& input);
+
 int main(int argc, char** argv)
 {
     CImg<float> input("images/lenaGrayHidden.bmp");
     CImgDisplay displayInput(input, "Image d'origine");
 
+    // Pixel qui font objet du traitement
     std::vector< std::pair< unsigned int, unsigned int > > mask = getMask(input);
 
     std::cout << "Nombre de pixel à reconstituer : " << mask.size() << std::endl;
+
+    // Initialisation random
+    CImg<float> finalImage(input);
+    randomInitMask(mask, finalImage);
 	
-    while (!displayInput.is_closed())
+    // Algo
+    // ...
+
+    CImgDisplay displayFinalImage(finalImage, "Image resultat");
+    while (!displayInput.is_closed() || !displayFinalImage.is_closed())
 	{
         displayInput.wait();
 	}
@@ -44,4 +63,19 @@ std::vector< std::pair< unsigned int, unsigned int > > getMask(const CImg<>& inp
     }
 
     return mask;
+}
+
+void randomInitMask(const std::vector< std::pair< unsigned int, unsigned int > >& mask, CImg<>& input)
+{
+    // Pour chaque pixel du masque
+    for (const auto& pixel : mask)
+    {
+        unsigned int x = mt() % (input.width());
+        unsigned int y = mt() % (input.height());
+
+        // Initialise la couleur du pixel avec un pixel aléatoire de l'image
+        input(pixel.first, pixel.second, 0) = input(x, y, 0);
+        input(pixel.first, pixel.second, 1) = input(x, y, 1);
+        input(pixel.first, pixel.second, 2) = input(x, y, 2);
+    }
 }
