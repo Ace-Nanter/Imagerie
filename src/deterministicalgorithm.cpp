1ef75534ff64f3ebdd83914eb33bf7d25f0c5431
Alexandre Rabérin <kernelith@live.fr>
2017-01-20 11:07:11 +0100

    Improve conception of algorithm (more object oriented)

---
+++ src/deterministicalgorithm.cpp
@@ -0,0 +1,117 @@
+#include "deterministicalgorithm.h"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include "random.h"
+
+DeterministicAlgorithm::DeterministicAlgorithm(CImg<> input, unsigned int nbIteration, bool verbose, bool produceStats)
+    : AbstractAlgorithm(input, nbIteration, verbose, produceStats)
+{
+    computeMask();
+    randomInitMask();
+}
+
+void DeterministicAlgorithm::computeMask()
+{
+    // Add every pixels in the image that should be reconstructed
+    cimg_forXY(m_image, x, y)
+    {
+        // Blank pixels
+        if (m_image(x, y/*, 0*/) == 255 /*&& input(x, y, 1) == 255 && input(x, y, 2) == 255*/)
+            m_mask.push_back({x, y});
+        else
+            m_outMask.push_back({x, y});
+    }
+}
+
+void DeterministicAlgorithm::randomInitMask()
+{
+    const unsigned int nbPixels = m_outMask.size();
+
+    // For each pixel of the mask
+    for (const auto& pixel : m_mask)
+    {
+        unsigned int index = mt() % (nbPixels);
+        const auto& seedPixel = m_outMask[index];
+
+        // Initialize the color of the pixel to a random pixel color in the seed image
+        m_image(pixel.first, pixel.second/*, 0*/) = m_image(seedPixel.first, seedPixel.second/*, 0*/);
+    }
+}
+
+void DeterministicAlgorithm::exec()
+{
+    double lastEnergy = std::numeric_limits<double>::max();
+
+    for (unsigned int i = 0 ; i < m_nbIterations ; ++i)
+    {
+        double energy = 0;
+
+        for (const auto& pixel : m_mask)
+        {
+            std::pair<unsigned int, unsigned int> bestMatch(0, 0);
+            double lowestDist = std::numeric_limits<double>::max();
+            CImg_3x3(I, float);
+
+            unsigned int i = 0;
+            cimg_for3x3(m_image, x, y, 0, 0, I, float)
+            {
+                auto seedPixelCoord = std::pair<unsigned int, unsigned int>(x, y);
+                // Pixel is outside mask => skip it
+                if (m_outMask[i] != seedPixelCoord) continue; else ++i;
+
+                // Treatments
+                const double diffIpp = m_image(pixel.first - 1, pixel.second - 1) - Ipp;
+                const double diffIcp = m_image(pixel.first    , pixel.second - 1) - Icp;
+                const double diffInp = m_image(pixel.first + 1, pixel.second - 1) - Inp;
+
+                const double diffIpc = m_image(pixel.first - 1, pixel.second) - Ipc;
+                //const double diffIcc = m_image(pixel.first    , pixel.second) - Icc;    // Current pixel not in neighbohood
+                const double diffInc = m_image(pixel.first + 1, pixel.second) - Inc;
+
+                const double diffIpn = m_image(pixel.first - 1, pixel.second + 1) - Ipn;
+                const double diffIcn = m_image(pixel.first    , pixel.second + 1) - Icn;
+                const double diffInn = m_image(pixel.first + 1, pixel.second + 1) - Inn;
+
+                double neighborhoodDist = diffIpp*diffIpp + diffIcp*diffIcp + diffInp*diffInp
+                                        + diffIpc*diffIpc /*+ diffIcc*diffIcc*/ + diffInc*diffInc
+                                        + diffIpn*diffIpn + diffIcn*diffIcn + diffInn*diffInn;
+
+                // If best neighorhood
+                if (neighborhoodDist < lowestDist)
+                {
+                    lowestDist = neighborhoodDist;
+                    bestMatch = { x, y };
+                }
+            }
+
+            energy += lowestDist;
+
+            // Set new pixel color
+            m_image(pixel.first, pixel.second) = m_image(bestMatch.first, bestMatch.second);
+        }
+
+        // Iteration results
+        double ratio = (lastEnergy - energy) / double(lastEnergy);
+        ratio = ratio > 0 ? ratio : -ratio;
+
+        // Stats and verbose
+        if (m_verbose)
+        {
+            if (m_fileStats)
+            {
+                std::stringstream ss;
+                ss << "./loop" << i;
+                std::ofstream ofs(ss.str(), std::ios::trunc | std::ios::out);
+                ofs << "Last Energy : " << lastEnergy << "\nEnergy : " << energy << "\nRatio : " << ratio << "\n\n";
+                ofs.close();
+            }
+
+            std::cout << "Loop : " << i << "\nLast Energy : " << lastEnergy << "\nEnergy : " << energy << "\nRatio : " << ratio << "\n" << std::endl;
+        }
+
+        lastEnergy = energy;
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,200 +1,44 @@
 #include <cstdlib>
 
-#include <fstream>
 #include <iostream>
-#include <sstream>
-#include <vector>
 
 #include "CImg.h"
 
-#include "random.h"
+#include "deterministicalgorithm.h"
 
 using namespace cimg_library;
-
-/**
- * @brief Recover all pixels coordinates that need reconstruction.
- * @param input Input image.
- * @param mask List of pixel coordinates corresponding to the mask.
- * @param outMask List of pixel coordinates out of the mask.
- */
-void getMask(const CImg<>& input,
-             std::vector< std::pair< unsigned int, unsigned int > >& mask,
-             std::vector< std::pair< unsigned int, unsigned int > >& outMask);
-
-/**
- * @brief Initialize all pixel from mask to a random value from input image.
- * @param mask List of pixel coordinates corresponding to the mask.
- * @param outMask List of pixel coordinates out of the mask.
- * @param input Input image.
- */
-void randomInitMask(const std::vector< std::pair< unsigned int, unsigned int > >& mask,
-                    const std::vector< std::pair< unsigned int, unsigned int > >& outMask,
-                    CImg<>& input);
-
-/**
- * @brief Use the deterministic method to emplace mask pixels.
- * @param mask List of pixel coordinates corresponding to the mask.
- * @param outMask List of pixel coordinates out of the mask.
- * @param image Image that will be modified.
- */
-void deterministicMethod(const std::vector< std::pair< unsigned int, unsigned int > >& mask,
-                         const std::vector< std::pair< unsigned int, unsigned int > >& outMask,
-                         CImg<>& image);
-
-// Verbose mode
-bool verbose;
-bool fileStats;
-
-// Number of iterations
-unsigned int nbIterations;
 
 /// MAIN ///
 int main(int argc, char** argv)
 {
-    verbose = cimg_option("-v", false, "Verbose mode");
-    fileStats = cimg_option("-f", false, "Write stats to file");
+    bool verbose = cimg_option("-v", false, "Verbose mode");
+    bool fileStats = cimg_option("-f", false, "Write stats to file");
     bool saveResult = cimg_option("-s", false, "Save result to file");
     const char* inputFile = cimg_option("-if", "images/lenaGrayHiddenSmall.bmp", "Input file name");
     const char* outputFile = cimg_option("-of", "output.bmp", "Output file name");
-    nbIterations = cimg_option("-n", 5, "Number of iterations");
+    unsigned int nbIterations = cimg_option("-n", 5, "Number of iterations");
 
     CImg<float> input = CImg<float>(inputFile).channel(0);
     CImgDisplay displayInput(input, "Input Image");
 
-    // Pixels that need treatment
-    std::vector< std::pair< unsigned int, unsigned int > > mask;
-    // Other pixels
-    std::vector< std::pair< unsigned int, unsigned int > > outMask;
-    getMask(input, mask, outMask);
-
-    std::cout << "Number of pixel to reconstitute: " << mask.size() << std::endl;
-
-    // Random initialization
-    CImg<float> finalImage(input);
-    randomInitMask(mask, outMask, finalImage);
+    // Create algorithm
+    AbstractAlgorithm* algo = new DeterministicAlgorithm(input, nbIterations, verbose, fileStats);
 	
     // Algo
-    deterministicMethod(mask, outMask, finalImage);
+    algo->exec();
 
     // Results
+    const CImg<>& result = algo->getResult();
     if (saveResult)
-        finalImage.save(outputFile);
+        result.save(outputFile);
 
-    CImgDisplay displayFinalImage(finalImage, "Result Image");
+    CImgDisplay displayFinalImage(result, "Result Image");
     while (!displayInput.is_closed() || !displayFinalImage.is_closed())
     {
         displayInput.wait();
     }
 
+    delete algo;
+
     return EXIT_SUCCESS;
-}
-
-
-void getMask(const CImg<>& input,
-             std::vector< std::pair< unsigned int, unsigned int > >& mask,
-             std::vector< std::pair< unsigned int, unsigned int > >& outMask)
-{
-    // Add every pixels in the image that should be reconstructed
-    cimg_forXY(input, x, y)
-    {
-        // Blank pixels
-        if (input(x, y/*, 0*/) == 255 /*&& input(x, y, 1) == 255 && input(x, y, 2) == 255*/)
-            mask.push_back({x, y});
-        else
-            outMask.push_back({x, y});
-    }
-}
-
-void randomInitMask(const std::vector< std::pair< unsigned int, unsigned int > >& mask,
-                    const std::vector< std::pair< unsigned int, unsigned int > >& outMask,
-                    CImg<>& input)
-{
-    const unsigned int nbPixels = outMask.size();
-
-    // For each pixel of the mask
-    for (const auto& pixel : mask)
-    {
-        unsigned int index = mt() % (nbPixels);
-        const auto& seedPixel = outMask[index];
-
-        // Initialize the color of the pixel to a random pixel color in the seed image
-        input(pixel.first, pixel.second/*, 0*/) = input(seedPixel.first, seedPixel.second/*, 0*/);
-    }
-}
-
-void deterministicMethod(const std::vector< std::pair< unsigned int, unsigned int > >& mask,
-                         const std::vector< std::pair< unsigned int, unsigned int > >& outMask,
-                         CImg<>& image)
-{
-    double lastEnergy = std::numeric_limits<double>::max();
-
-    for (unsigned int i = 0 ; i < nbIterations ; ++i)
-    {
-        double energy = 0;
-
-        for (const auto& pixel : mask)
-        {
-            std::pair<unsigned int, unsigned int> bestMatch(0, 0);
-            double lowestDist = std::numeric_limits<double>::max();
-            CImg_3x3(I, float);
-
-            unsigned int i = 0;
-            cimg_for3x3(image, x, y, 0, 0, I, float)
-            {
-                auto seedPixelCoord = std::pair<unsigned int, unsigned int>(x, y);
-                // Pixel is outside mask => skip it
-                if (outMask[i] != seedPixelCoord) continue; else ++i;
-
-                // Treatments
-                const double diffIpp = image(pixel.first - 1, pixel.second - 1) - Ipp;
-                const double diffIcp = image(pixel.first    , pixel.second - 1) - Icp;
-                const double diffInp = image(pixel.first + 1, pixel.second - 1) - Inp;
-
-                const double diffIpc = image(pixel.first - 1, pixel.second) - Ipc;
-                //const double diffIcc = image(pixel.first    , pixel.second) - Icc;    // Current pixel not in neighbohood
-                const double diffInc = image(pixel.first + 1, pixel.second) - Inc;
-
-                const double diffIpn = image(pixel.first - 1, pixel.second + 1) - Ipn;
-                const double diffIcn = image(pixel.first    , pixel.second + 1) - Icn;
-                const double diffInn = image(pixel.first + 1, pixel.second + 1) - Inn;
-
-                double neighborhoodDist = diffIpp*diffIpp + diffIcp*diffIcp + diffInp*diffInp
-                                        + diffIpc*diffIpc /*+ diffIcc*diffIcc*/ + diffInc*diffInc
-                                        + diffIpn*diffIpn + diffIcn*diffIcn + diffInn*diffInn;
-
-                // If best neighorhood
-                if (neighborhoodDist < lowestDist)
-                {
-                    lowestDist = neighborhoodDist;
-                    bestMatch = { x, y };
-                }
-            }
-
-            energy += lowestDist;
-
-            // Set new pixel color
-            image(pixel.first, pixel.second) = image(bestMatch.first, bestMatch.second);
-        }
-
-        // Iteration results
-        double ratio = (lastEnergy - energy) / double(lastEnergy);
-        ratio = ratio > 0 ? ratio : -ratio;
-
-        // Stats and verbose
-        if (verbose)
-        {
-            if (fileStats)
-            {
-                std::stringstream ss;
-                ss << "./loop" << i;
-                std::ofstream ofs(ss.str(), std::ios::trunc | std::ios::out);
-                ofs << "Last Energy : " << lastEnergy << "\nEnergy : " << energy << "\nRatio : " << ratio << "\n\n";
-                ofs.close();
-            }
-
-            std::cout << "Loop : " << i << "\nLast Energy : " << lastEnergy << "\nEnergy : " << energy << "\nRatio : " << ratio << "\n" << std::endl;
-        }
-
-        lastEnergy = energy;
-    }
 }
